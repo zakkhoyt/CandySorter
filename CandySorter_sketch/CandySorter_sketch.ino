1dd6942f00ff5cba54049d42efe13e9a6f155abb
Zakk Hoyt <zacharyhoyt@zakkimac.local>
2014-03-01 22:13:38 -0800

    Updated sketch

--- CandySorter_sketch/CandySorter_sketch.ino
+++ CandySorter_sketch/CandySorter_sketch.ino
@@ -1,10 +1,28 @@
-// Sweep
-// by BARRAGAN <http://barraganstudio.com> 
-// This example code is in the public domain.
 
+// Redbear includes
+#include <SPI.h>
+#include <boards.h>
+#include <ble_shield.h>
+#include <services.h>
 
+// VWW includes
 #include <Servo.h> 
 
+//#define VWW_ENABLE_BLE 1
+#define VWW_ENBABLE_SERVOS 1
+
+
+
+// Readbear Defines
+#define DIGITAL_OUT_PIN    4
+#define DIGITAL_IN_PIN     5
+#define PWM_PIN            6
+#define SERVO_PIN          7
+#define ANALOG_IN_PIN      A5
+
+//Servo myservo;
+
+// VWW Defines
 const unsigned int kEntryServo = 9;
 const unsigned int kExitServo = 10;
 const unsigned int kPickupPosition = 160;
@@ -17,24 +35,135 @@ const unsigned int kNumChoices = 12;
 
 Servo entryServo;
 Servo exitServo;
-               
 int pos = 0;    // variable to store the servo position 
- 
-void setup() 
-{ 
+
+
+
+void setup()
+{
+  
+#if defined(VWW_ENABLE_BLE)
+  // Default pins set to 9 and 8 for REQN and RDYN
+  // Set your REQN and RDYN here before ble_begin() if you need
+  //ble_set_pins(3, 2);
+  
+  // Init. and start BLE library.
+  ble_begin();
+  
+  // Enable serial debug
+  Serial.begin(57600);
+  
+  pinMode(DIGITAL_OUT_PIN, OUTPUT);
+  pinMode(DIGITAL_IN_PIN, INPUT);
+  
+  // Default to internally pull high, change it if you need
+  digitalWrite(DIGITAL_IN_PIN, HIGH);
+  //digitalWrite(DIGITAL_IN_PIN, LOW);
+  
+//  myservo.attach(SERVO_PIN);
+  
+#endif
+
+#if defined(VWW_ENBABLE_SERVOS)
   Serial.begin(9600);
   Serial.println("Init entry servo");
   entryServo.attach(kEntryServo);
   Serial.println("Init exit servo");
   exitServo.attach(kExitServo);
-} 
- 
- 
-void loop() 
-{ 
-  
+#endif
+}
 
-//  for(pos = 0; pos < 160; pos += 1)  // goes from 0 degrees to 180 degrees 
+void loop()
+{
+  
+#if defined(VWW_ENABLE_BLE)
+  static boolean analog_enabled = false;
+  static byte old_state = LOW;
+  
+  // If data is ready
+  while(ble_available())
+  {
+    // read out command and data
+    byte data0 = ble_read();
+    byte data1 = ble_read();
+    byte data2 = ble_read();
+    
+    if (data0 == 0x01)  // Command is to control digital out pin
+    {
+      if (data1 == 0x01)
+        digitalWrite(DIGITAL_OUT_PIN, HIGH);
+      else
+        digitalWrite(DIGITAL_OUT_PIN, LOW);
+    }
+    else if (data0 == 0xA0) // Command is to enable analog in reading
+    {
+      if (data1 == 0x01)
+        analog_enabled = true;
+      else
+        analog_enabled = false;
+    }
+    else if (data0 == 0x02) // Command is to control PWM pin
+    {
+      analogWrite(PWM_PIN, data1);
+    }
+    else if (data0 == 0x03)  // Command is to control Servo pin
+    {
+//      myservo.write(data1);
+    }
+    else if (data0 == 0x04)
+    {
+      analog_enabled = false;
+//      myservo.write(0);
+      analogWrite(PWM_PIN, 0);
+      digitalWrite(DIGITAL_OUT_PIN, LOW);
+    }
+  }
+  
+  if (analog_enabled)  // if analog reading enabled
+  {
+    // Read and send out
+    uint16_t value = analogRead(ANALOG_IN_PIN); 
+    ble_write(0x0B);
+    ble_write(value >> 8);
+    ble_write(value);
+  }
+  
+  // If digital in changes, report the state
+  if (digitalRead(DIGITAL_IN_PIN) != old_state)
+  {
+    old_state = digitalRead(DIGITAL_IN_PIN);
+    
+    if (digitalRead(DIGITAL_IN_PIN) == HIGH)
+    {
+      ble_write(0x0A);
+      ble_write(0x01);
+      ble_write(0x00);    
+    }
+    else
+    {
+      ble_write(0x0A);
+      ble_write(0x00);
+      ble_write(0x00);
+    }
+  }
+  
+  if (!ble_connected())
+  {
+    analog_enabled = false;
+    digitalWrite(DIGITAL_OUT_PIN, LOW);
+  }
+  
+  // Allow BLE Shield to send/receive data
+  ble_do_events();  
+#endif
+
+
+
+
+#if defined(VWW_ENABLE_SERVOS)  
+  
+  
+  //  for(pos = 0; pos < 160; pos += 1)  // goes from 0 degrees to 180 degrees 
 //  {                                  // in steps of 1 degree 
 //    entryServo.write(pos);              // tell servo to go to position in variable 'pos' 
 //    exitServo.write(pos);
@@ -64,7 +193,8 @@ void loop()
     delay(200);
   }
 
-} 
+#endif
+}
 
 
 
@@ -94,3 +224,5 @@ void dispenseCandy(unsigned int pos, unsigned int totalPositions){
 
   exitServo.write(p);
 }
+
+
